8c3a1fe654ebbcf5e1453f4c9617d75d144002de
Aubrey Graves <grav0165@gmail.com>
2023-05-16 14:17:50 -0500

    completed 2 and 3, fixed infinite loop again

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -42,16 +42,36 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for ( let i=0; i<stars.length; i++) {
+  console.log('Some stars:', stars[i]);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+let i=0;
+while ( i<stars.length) {
+  console.log('Some stars using while:', stars[i]);
+  i++
+}
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+let index = 0;
+const max = 6;
+while ( index < max) {
+  console.log(index);
+  index++;
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let countDown = 10;
+const lowest = 4;
+while (countDown > lowest) {
+  console.log(countDown);
+  countDown--;
+}
